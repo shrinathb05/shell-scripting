346d2324b3125ad53c4a30d400c9a77c42e400fc
shrinath <virusking045@gmail.com>
2025-12-24 18:46:10 +0530

    new changes done

--- linux/scripts/22_Dec_25/variable_and_userinput.sh
+++ linux/scripts/22_Dec_25/variable_and_userinput.sh
@@ -22,7 +22,7 @@ fi
 echo
 echo "-------------------------------------------------------------------------------------------------"
 echo
-#comment
+
 
 # Accept a port number and check if it's open
 read -p "Enter the port: " port
@@ -36,9 +36,36 @@ fi
 echo
 echo "-------------------------------------------------------------------------------------------------"
 echo
-comment
+
 
 # Read username and display it's home directory
 read -p "Enter username: " user
 eval echo "~$user"
 
+echo
+echo "-------------------------------------------------------------------------------------------------"
+echo
+
+
+# Take input for a service name and check it's status
+read -p "Enter Service name: " svc
+sudo systemctl status $svc
+if [[ $? -eq 0 ]]; then
+	echo "The $svc is running !!!"
+else
+	echo " The $svc is not running"
+fi
+echo
+echo "-------------------------------------------------------------------------------------------------"
+echo
+comment
+
+# Ask for a package name and check if installed
+read -p "Enter package: " pkg
+dpkg -l | grep "$pkg"
+if [[ $? -eq 0 ]]; then
+	echo "Package $pkg is installed"
+else
+	echo "Package $pkg is not installed"
+fi
+echo	
