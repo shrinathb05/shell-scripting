53ab879129a89e80fed48c7e440078f40fd2c9e0
Shrinath Bhosale <139325389+shrinathb05@users.noreply.github.com>
2025-12-28 20:57:19 +0530

    new python scripts added

---
+++ 28-12-2025/01_first.py
@@ -0,0 +1,5 @@
+# This is my first program to printing the msg
+print ("My first python script")
+print ("")
+print (" I am excited to learn the python")
+
---
+++ 28-12-2025/07_operators.py
@@ -0,0 +1,63 @@
+# Python Operators
+
+# 01.Arithmetic operators
+x = 2
+y = 4
+
+# Addition
+total = x + y
+print(total)
+
+# Substraction
+total = x - y
+print(total)
+
+# Multiplication
+total = x * y
+print(total)
+
+# Division
+total = y / x
+print(total)
+
+# Modulus
+total = y % x
+print(total)
+
+# Power square
+total = y ** x
+print(total)
+
+#----------------------------------------------------------
+
+# Comparision Operator
+# This operator will print values in boolen format like True or False
+a = 30
+b = 60
+
+out = a < b     # less than 
+print(out)
+
+out = a > b     # greater than 
+print(out)
+
+out = a != b    # not equal to
+print(out)
+
+out = a <= b    # less than equal
+print(out)
+
+out = a >= b    # greater than equal
+print(out)
+
+#----------------------------------------------------------
+
+# Assignment Operator
+c = 3
+d = 10
+
+# c += d # c is similar euql to c = c + d
+# print(c)
+
+c -= d # c is similar euql to c = c - d
+print(c)
