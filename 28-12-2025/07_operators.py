f8d8d2bb27d8eeb752942cd09d37ee0ada921189
Shrinath Bhosale <139325389+shrinathb05@users.noreply.github.com>
2025-12-28 21:28:45 +0530

    Add files via upload

--- 28-12-2025/07_operators.py
+++ 28-12-2025/07_operators.py
@@ -1,6 +1,9 @@
 # Python Operators
 
 # 01.Arithmetic operators
+from ast import Or
+
+
 x = 2
 y = 4
 
@@ -61,3 +64,58 @@ d = 10
 
 c -= d # c is similar euql to c = c - d
 print(c)
+
+#----------------------------------------------------------
+
+# Logical Operators
+# AND (if once condition is true and another is false then it will return as false)
+# OR (If once condition os true and another is false then it will return true)
+
+# Examples:-
+'''
+TRUE or TRUE = TRUE
+TRUE or FALSE = TRUE
+FALSE or TRUE = TRUE
+FALSE or FALSE = FALSE
+FALSE and TRUE = FALSE
+TRUE and FALSE = FALSE
+'''
+
+a = 40
+b = 60
+
+x = 2
+y = 3
+
+out = (a < b) or (x > y) # One condition will be TRUE
+print(out)
+
+out = (a > b) or (x < y) # One condition will be TRUE
+print(out)
+
+out = (a > b) or (x > y) # One condition will be FALSE
+print(out)
+
+
+out = (a < b) and (x > y) # One condition will be FALSE
+print(out)
+
+out = (a > b) and (x < y) # One condition will be FALSE
+print(out)
+
+out = (a > b) and (x > y) # One condition will be FALSE
+print(out)
+
+# NOT operator
+out = not(x < y) # If condition is TRUE the not operator will make it FALSE
+print(out)
+
+out = not(x > y) # If condition is FALSE the not operator will make it TRUE
+print(out)
+
+
+# Membership Operator
+
+first_tuple = (str1, "DevOps", 54, num1, 5.4)
+ans = "DevOps"
+
