ed97d367759e1dfa17aca97018d6fd261899da4d
shrinath <virusking045@gmail.com>
2025-12-31 11:54:22 +0530

    new changes done and created some new folders

---
+++ scripting/python/28-12-2025/04_variables.py
@@ -0,0 +1,31 @@
+# Variable arguments
+var1="python"
+var2=75
+var3=3.5
+
+#printing variables
+print(var1)
+print(var2)
+print(var3)
+
+# Multiple assignments
+a = b = c = 65
+print(a)
+print(b)
+print(c)
+
+# Multiple values assigned to multiple variables
+
+w, x, y, z ="alpha", "beta", 12, 5.8
+
+print("W value is:" , w)
+print("X value is:" , x)
+print("Y value is:" , y)
+print("Z value is:" , z)
+
+print(type(w))
+print(type(x))
+print(type(y))
+print(type(z))
+
+
---
+++ scripting/python/main.py
@@ -0,0 +1,23 @@
+# print statement
+print("hi this is shrinath starting with the python scripting from today onwards")
+
+# variables
+var1="shri"
+print(f"hey hi {var1}! how are you doing")
+
+# f-strings
+# with the you can print the outputs very clearly
+lang="python"
+tool="Docker"
+print(f"hey {var1}, your favourite language is {lang} and you are currently working on the {tool} tool. That's great!")
+
+
+# taking input from the user
+name= input("Enter your name: ")
+print("Ok! your name is ", name)
+
+# interger value
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+print(num1)
+print(num2)
